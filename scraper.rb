c4fb3f452fac308ab21d1c6a19ee0c9c84648927
Tony Bowden <tony@tmtm.com>
2017-05-04 06:47:13 +0100

    rubocop -a

--- scraper.rb
+++ scraper.rb
@@ -1,29 +1,30 @@
 #!/bin/env ruby
 # encoding: utf-8
+# frozen_string_literal: true
 
 require 'scraperwiki'
 require 'nokogiri'
 require 'date'
 require 'open-uri'
 
-# require 'open-uri/cached'
-# require 'colorize'
-# require 'pry'
-# require 'csv'
+# require 'open-uri/cached'
+# require 'colorize'
+# require 'pry'
+# require 'csv'
 
 def noko(url)
-  Nokogiri::HTML(open(url).read) 
+  Nokogiri::HTML(open(url).read)
 end
 
 @WIKI = 'https://en.wikipedia.org'
 
 def wikilink(a)
-  return if a.attr('class') == 'new' 
+  return if a.attr('class') == 'new'
   @WIKI + a['href']
 end
 
 @terms = {
-  '2014' => "List_of_current_members_of_the_National_Assembly_of_Botswana",
+  '2014' => 'List_of_current_members_of_the_National_Assembly_of_Botswana',
 }
 
 @terms.each do |term, pagename|
@@ -31,25 +32,24 @@ end
   page = noko(url)
   added = 0
 
-  page.xpath('//table[.//th[text()[contains(.,"Constituency")]]]').each_with_index do |ct, i|
+  page.xpath('//table[.//th[text()[contains(.,"Constituency")]]]').each_with_index do |ct, _i|
     ct.xpath('tr[td]').each do |member|
       tds = member.xpath('td')
 
-      data = { 
-        name: tds[2].at_xpath('a') ? tds[2].xpath('a').text.strip : tds.first.text.strip,
-        wikiname: tds[2].xpath('a[not(@class="new")]/@title').text.strip,
-        wikipedia: tds[2].xpath('a[not(@class="new")]/@href').text.strip,
+      data = {
+        name:         tds[2].at_xpath('a') ? tds[2].xpath('a').text.strip : tds.first.text.strip,
+        wikiname:     tds[2].xpath('a[not(@class="new")]/@title').text.strip,
+        wikipedia:    tds[2].xpath('a[not(@class="new")]/@href').text.strip,
         constituency: tds[1].text.strip,
-        party: tds[4].at_xpath('a') ? tds[4].xpath('a').text.strip : tds.last.text.strip,
-        source: url,
-        term: term,
+        party:        tds[4].at_xpath('a') ? tds[4].xpath('a').text.strip : tds.last.text.strip,
+        source:       url,
+        term:         term,
       }
       data[:wikipedia].prepend @WIKI unless data[:wikipedia].empty?
       data[:constituency] = '' if data[:constituency].include?('Specially elected') or data[:constituency].include?('Ex officio')
       added += 1
-      ScraperWiki.save_sqlite([:name, :term], data)
+      ScraperWiki.save_sqlite(%i[name term], data)
     end
   end
   warn "Added #{added} for #{term}"
 end
-
